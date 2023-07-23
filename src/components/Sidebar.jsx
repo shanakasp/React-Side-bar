4f95c66b4484e43fb628ba75155605ba5ccabdc3
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-23 13:24:35 +0530

    modified Sidebar.jsx in components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -49,7 +49,7 @@ const Sidebar = () => {
                 className={
                     closeMenu === false
                         ? "profileContainer"
-                        : "profileContainer acive"
+                        : "profileContainer active"
                 }
             >
                 <img src={Profile} alt="profile" className="profile" />
